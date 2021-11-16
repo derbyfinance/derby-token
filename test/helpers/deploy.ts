4b39bf6389a10b8e51f3eee04af0c78963267b67
unknown <nickvanrossenberg@hotmail.com>
2021-11-16 11:40:18 +0100

    working on token tests

---
+++ test/helpers/deploy.ts
@@ -0,0 +1,10 @@
+import { deployContract } from 'ethereum-waffle';
+import { Signer, BigNumberish } from 'ethers';
+
+import TestTokenArtifact from './../../artifacts/contracts/token/TestToken.sol/TestToken.json';
+import { TestToken } from '@typechain/TestToken';
+
+export const deployTestToken = (deployerSign: Signer, name: string , symbol: string, cap: number): Promise<TestToken> => {
+  return (deployContract(deployerSign, TestTokenArtifact, [name, symbol, cap])) as Promise<any>; 
+  // Promise any nog aanpassen veranderd in nieuwe typechain versie
+};
---
+++ test/token.test.ts
@@ -0,0 +1,67 @@
+import chai, { expect } from "chai";
+import { Signer, Wallet } from "ethers"
+import { ethers, waffle , network} from "hardhat";
+import { Provider } from "web3/providers";
+import { deployTestToken} from "./helpers/deploy";
+import { getUSDCSigner, erc20, formatUSDC, parseUSDC, skipTimeDays } from './helpers/helpers';
+import { TestToken } from "@typechain/TestToken";
+import { ERC20 } from "@typechain/ERC20";
+
+// Still have to declare module
+const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
+
+const name = 'TestToken';
+const symbol = 'TST';
+const cap = 1_000_000;
+
+const initialSupply = 0;
+
+describe("Deploy Token Contract", async () => {
+  let testToken: TestToken, owner: Signer, recipient: Signer, USDCSigner: Signer, erc20USDC: ERC20;
+
+  beforeEach(async () => {
+    [owner, recipient] = await ethers.getSigners();
+    testToken = await deployTestToken(owner, name, symbol, cap);
+
+    // console.log(testToken)
+  });
+
+  it('has a name', async function () {
+    expect(await testToken.name()).to.equal(name);
+  });
+
+  it('has a symbol', async function () {
+    expect(await testToken.symbol()).to.equal(symbol);
+  });
+
+  it('has 18 decimals', async function () {
+    expect(await testToken.decimals()).to.equal(18);
+  });
+
+  describe('_mint', function () {
+    const amount = 100_000;
+
+    it('rejects a null account', async function () {
+      await expectRevert(
+      await testToken.mint(ZERO_ADDRESS, amount), 'ERC20: mint to the zero address',
+      );
+    });
+  
+    describe('for a non zero account', function () {
+      beforeEach('minting', async function () {
+        const  logs  = await testToken.mint(await recipient.getAddress(), amount);
+      });
+  
+      it('increments totalSupply', async function () {
+        const expectedSupply = initialSupply + amount;
+        expect(await testToken.totalSupply()).to.equal(expectedSupply);
+      });
+  
+      it('increments recipient balance', async function () {
+        expect(await testToken.balanceOf(await recipient.getAddress())).to.equal(amount);
+      });
+  
+    });
+  });
+});
